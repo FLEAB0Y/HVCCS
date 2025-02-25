47c39d324b55cf0c339d7c7662cc33bbaa0ee4a2
ztw <2415026224@qq.com>
2025-02-25 16:34:33 +0800

    delete img2vid function

--- Render.py
+++ Render.py
@@ -15,31 +15,6 @@ def clear_folder(folder_path):
         shutil.rmtree(folder_path)
     os.makedirs(folder_path)
 
-def images_to_video(image_folder, video_path, fps=30):
-    images = [img for img in os.listdir(image_folder) if img.endswith(".png") or img.endswith(".jpg")]
-    images.sort()  # 按名称排序
-
-    if not images:
-        print("No images found in the folder.")
-        return
-
-    # 获取第一张图片的尺寸
-    first_image_path = os.path.join(image_folder, images[0])
-    frame = cv2.imread(first_image_path)
-    height, width, layers = frame.shape
-
-    # 定义视频编码器和输出视频文件
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 使用 mp4 编码
-    video = cv2.VideoWriter(video_path, fourcc, fps, (width, height))
-
-    for image in images:
-        image_path = os.path.join(image_folder, image)
-        frame = cv2.imread(image_path)
-        video.write(frame)
-
-    video.release()
-    print(f"Video saved at {video_path}")
-
 def setup_render_environment(render_output_path, 
                              Avatar_path, 
                              camera_location,
