3719dc6e0a48e37d617aa17284b0dc5754c2e274
EdgeEnd <2415026224@qq.com>
2025-02-26 15:04:11 +0800

    daily update

--- stream_faceblendshape_sender.py
+++ stream_faceblendshape_sender.py
@@ -63,12 +63,8 @@ def main(server_addr='127.0.0.1', port_num =50051 , model_path='face_landmarker_
     client_thread = threading.Thread(target=run_client, args=(client,))
     client_thread.start()
     
-<<<<<<< HEAD
     running = True
     while cap.isOpened() and running:
-=======
-    while cap.isOpened():
->>>>>>> 1523028b9d8c1eb4f63b40c0d36cf230151a6f6f
         
         # 从相机从捕获一帧图片
         ret, frame = cap.read()
@@ -86,17 +82,10 @@ def main(server_addr='127.0.0.1', port_num =50051 , model_path='face_landmarker_
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
         # 异步检测
         detector.detect_async(mp_image, int(frame_timestamp_ms))
-<<<<<<< HEAD
         # 输入q结束捕获
         if cv2.waitKey(25) & 0xFF == ord('q'):
             print('exited')
             running = False
-=======
-        # 输入esc结束捕获
-        if cv2.waitKey(25) == 27:
-            print('exited')
-            break
->>>>>>> 1523028b9d8c1eb4f63b40c0d36cf230151a6f6f
         print(f"frame_timestamp_ms: {frame_timestamp_ms}")
         frame_timestamp_ms = int(time.time() * 1000)
 
@@ -104,4 +93,4 @@ def main(server_addr='127.0.0.1', port_num =50051 , model_path='face_landmarker_
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    main(server_addr='101.6.65.237', port_num=50051, model_path='face_landmarker_v2_with_blendshapes.task')
+    main(server_addr='183.173.48.193', port_num=50051, model_path='face_landmarker_v2_with_blendshapes.task')
