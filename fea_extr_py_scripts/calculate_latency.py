8e3f21c30c59454332b5e6e44f3e0ceb10e2cfad
LabServer <2415026224@qq.com>
2025-04-08 17:22:00 +0800

    daily update

--- fea_extr_py_scripts/calculate_latency.py
+++ fea_extr_py_scripts/calculate_latency.py
@@ -1,45 +1,98 @@
-import ntplib
+import grpc
+import data_stream_pb2
+import data_stream_pb2_grpc
 import time
-import subprocess
+import threading
+from THStreamData import THStreamDataPayload, THDataWarehouse
+from calculate_latency import init_time
 
-# 初始化时间，获取本地时间戳与网络时间戳的差值 current_web_time = int(time.time() * 1000) + diff
-def init_time():
-    while True:
+class THStreamClient:
+    def __init__(self, host='127.0.0.1', port=50051):
+        self.channel = grpc.insecure_channel(f'{host}:{port}')
+        self.stub = data_stream_pb2_grpc.THStreamServiceStub(self.channel)
+        self.seq_no = 0
+        # 数据缓存
+        self.send_data_buffer = THDataWarehouse(capacity= 100)
+        self.lock = threading.Lock()
+
+    def next_seq_no(self):
+        with self.lock:
+            self.seq_no += 1
+        return str(self.seq_no)
+
+    def send_data(self):
         try:
-            # 测量网络延迟
-            ping_result = subprocess.run(
-                ["ping", "-c", "1", "pool.ntp.org"],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                text=True
-            )
-            if ping_result.returncode != 0:
-                raise Exception("Ping 失败")
-            
-            # 提取 ping 时间 (以毫秒为单位)
-            ping_output = ping_result.stdout
-            latency_line = [line for line in ping_output.split("\n") if "time=" in line]
-            if not latency_line:
-                raise Exception("无法解析 ping 输出")
-            latency = float(latency_line[0].split("time=")[1].split(" ")[0])
-            
-            # 获取网络时间
-            ntp_client = ntplib.NTPClient()
-            respinse = ntp_client.request('pool.ntp.org', version=3)
-            web_timestamp = int(respinse.tx_time * 1000)
-            local_timestamp = int(time.time() * 1000)
-            break
-        except Exception as e:
-            print(f"从 pool.ntp.org 请求时间戳失败: {e}")
-    
-    # 将网络延迟的一半加入时间差
-    diff = web_timestamp - local_timestamp + int(latency / 2)
-    return diff
-    
+            send_data = self.request_generator()
+            if not send_data:
+                return
+            response_iterator = self.stub.BidirectionalStream(send_data)
+            for response in response_iterator:
+                print(f"Received response: retCode={response.retCode}, retMsg={response.retMsg}")
+        except grpc.RpcError as e:
+            print(f"gRPC error: {e}")
+
+    def request_generator(self):
+        if self.send_data_buffer.get_size() == 0:
+            return None
+        else:
+            for i in range(self.send_data_buffer.get_size()):
+                one_data = self.send_data_buffer.get_items()
+                seq_no = self.next_seq_no()
+                yield data_stream_pb2.THStreamRequest(seqNo=seq_no,
+                                                      rgbData=one_data.rgb_data,
+                                                      pointData=one_data.point_data,
+                                                      faceData=one_data.face_data,
+                                                      limbData=one_data.limb_data,
+                                                      extData=one_data.ext_data,
+                                                      extDesc=one_data.ext_desc)
+
+    def run(self, interval=1./30.): # interval = 1./30.
+        """
+        :param interval: 1秒30帧数据
+        :return:
+        """
+        try:
+            while True:
+                self.send_data()
+                time.sleep(interval)
+        except KeyboardInterrupt:
+            print("Client stopped")
+
+def run_client(client):
+    client.run()
+
 if __name__ == '__main__':
-    local_time = int(time.time() * 1000)
+    # 初始化时间差
     diff = init_time()
-    # 计算本地时间与网络时间的差值
-    print(f"本地时间戳: {local_time}")
-    print(f"网络时间戳: {local_time + diff}")
-    print(f"时间差: {diff}")
+    print(f"时间差: {diff} ms")
+    
+    # Labserver: 101.6.65.237
+    # local: 127.0.0.1
+    client = THStreamClient(host='127.0.0.1', port=50051)
+    client_thread = threading.Thread(target=run_client, args=(client,))
+    client_thread.start()
+
+    while True:
+        
+        # 缓冲区满了就等待
+        buffer_size = client.send_data_buffer.get_size()
+        while buffer_size >= 10:
+            time.sleep(0.1)
+            buffer_size = client.send_data_buffer.get_size()
+        # 生成250,0000字节的数据
+        large_data = b'\x00' * 2500000
+        # 获取当前时间戳（毫秒）
+        timestamp_ms = int(time.time() * 1000) 
+        # 创建数据负载
+        payload = THStreamDataPayload(rgb_data=b'\x01', 
+                                       point_data=large_data, 
+                                       face_data=b'\x03', 
+                                       limb_data=b'\x04',
+                                       ext_data=b'\x05', 
+                                       ext_desc=f"{str(timestamp_ms)}")
+        client.send_data_buffer.add_item(payload)
+
+
+
+
+
